43cb960b20e69d6c50e80bb3a18eab5ae470db7b
Vu Luong <vul@Liberty-MacBook-Pro-M1.local>
2024-08-18 10:20:53 +0700

    fix build

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -34,7 +34,7 @@ const Contact = () => {
                     {/* Form */}
                     <div className="xl:w-[54%] order-2 xl:order-none">
                         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-                            <h3 className="text-4xl text-accent">Let's work together</h3>
+                            <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
                             <p className="text-white/60">
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             </p>
